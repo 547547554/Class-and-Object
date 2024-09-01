a601397f82bc1fefc267ce00872af87c6efaaff2
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-01 09:34:56 +0530

    Add files via upload

---
+++ oops2.cpp
@@ -0,0 +1,72 @@
+
+#include<iostream>
+using namespace std;
+// class student{
+    //  public:
+    // string name;
+    // int age , roll_no;
+    // string grade;
+    
+    // function getter and setter -- private access for we make public / this only for access in class
+    // this function are for set
+//     public:
+//     string name;
+//     int age , roll_no;
+//     string grade;
+    
+//    void setname(string s)
+//    {
+//         name=s;
+//     }
+//     void setage(int a)
+//     {
+//         age=a;
+//     }
+//     void setroll_no(int r)
+//     {
+//         roll_no=r;
+//     }
+//     void setgrade(string g)
+//     {
+//         grade=g;
+//     }
+//     void setname()
+//     {
+//         cout<<name<<endl;
+//     }
+// };
+// int main(){
+//     student s1;
+//     s1.name="rohit";
+//     s1.age=15;
+//     s1.roll_no=123;
+//     s1.grade="A++";
+//     s1.setname();
+   
+// }
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// this program for get
+class student{
+    //  public:
+    // string name;
+    // int age , roll_no;
+    // string grade
+ public:
+    string name;
+    int age , roll_no;
+    string grade;
+    
+   void getname(){
+    cout
+   }
+};
+int main(){
+    student s1;
+    s1.name="rohit";
+    s1.age=15;
+    s1.roll_no=123;
+    s1.grade="A++";
+    s1.getname();
+   
+}
