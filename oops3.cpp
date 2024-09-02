9df84144b6425f5cb038637a0a7c240487682f7f
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-02 22:17:38 +0530

    Add files via upload

---
+++ oops3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string name ;
+    int age , roll_no;
+    string grade;
+    cin>>name>>age>>roll_no>>grade;
+    
+    string name2;
+    int age2,roll_no2;
+    string grade2;
+    cin>>name2>>age2>>roll_no>>grade;
+}
+void function(){
+    string name2;
+    int age2,roll_no;
+    string grade;
+}
+
